b89510fe803c327c27734a6de47fdbe0027407f5
aaron <aaron@abelard.com>
2025-12-08 18:34:02 -0500

    switch to go-tree-sitter for parsing instead of regex

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.25.5
 
 require gopkg.in/yaml.v2 v2.4.0
 
-require github.com/bmatcuk/doublestar/v4 v4.9.1 // indirect
+require (
+	github.com/bmatcuk/doublestar/v4 v4.9.1 // indirect
+	github.com/smacker/go-tree-sitter v0.0.0-20240827094217-dd81d9e9be82 // indirect
+)
--- internal/parser/js_parser.go
+++ internal/parser/js_parser.go
@@ -2,8 +2,10 @@ package parser
 
 import (
 	"os"
-	"regexp"
 	"strings"
+
+	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/smacker/go-tree-sitter/javascript"
 
 	"codemap/internal/types"
 )
@@ -11,63 +13,139 @@ import (
 // JSParser implements the Parser interface for JavaScript and TypeScript files
 type JSParser struct{}
 
-// Parse extracts definitions from a JS/TS source file using regex patterns
+// Parse extracts definitions from a JS/TS source file using AST parsing
 func (p *JSParser) Parse(filePath string) ([]types.Definition, error) {
-	content, err := os.ReadFile(filePath)
+	src, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 
-	lines := strings.Split(string(content), "\n")
+	parser := sitter.NewParser()
+	parser.SetLanguage(javascript.GetLanguage())
+	tree := parser.Parse(nil, src)
+
+	lines := strings.Split(string(src), "\n")
 	var definitions []types.Definition
 
-	// Regex patterns for different constructs
-	funcRegex := regexp.MustCompile(`^\s*function\s+(\w+)\s*\(`)
-	arrowRegex := regexp.MustCompile(`^\s*(?:const|let|var)\s+(\w+)\s*=\s*(?:\([^)]*\)\s*=>|function)`)
-	classRegex := regexp.MustCompile(`^\s*class\s+(\w+)`)
-	interfaceRegex := regexp.MustCompile(`^\s*interface\s+(\w+)`)
+	walkTree(tree.RootNode(), src, lines, filePath, &definitions)
 
-	for i, line := range lines {
-		lineNum := i + 1
+	return definitions, nil
+}
 
-		var defType, name string
-		var signature string
-
-		if matches := funcRegex.FindStringSubmatch(line); matches != nil {
-			defType = "function"
-			name = matches[1]
-			signature = strings.TrimSpace(line)
-		} else if matches := arrowRegex.FindStringSubmatch(line); matches != nil {
-			defType = "function"
-			name = matches[1]
-			signature = strings.TrimSpace(line)
-		} else if matches := classRegex.FindStringSubmatch(line); matches != nil {
-			defType = "type"
-			name = matches[1]
-			signature = strings.TrimSpace(line)
-		} else if matches := interfaceRegex.FindStringSubmatch(line); matches != nil {
-			defType = "type"
-			name = matches[1]
-			signature = strings.TrimSpace(line)
+func walkTree(node *sitter.Node, src []byte, lines []string, filePath string, definitions *[]types.Definition) {
+	switch node.Type() {
+	case "function_declaration":
+		if def := extractFunction(node, src, lines, filePath); def != nil {
+			*definitions = append(*definitions, *def)
 		}
-
-		if defType != "" {
-			comment := extractJSComment(lines, i)
-			sig := normalizeWhitespace(signature)
-			def := types.Definition{
-				Type:      defType,
-				Name:      name,
-				Line:       lineNum,
-				LineEnd:    lineNum, // TODO: implement proper end detection
-				Id:        computeId(filePath, name, sig),
-				Signature: sig,
-				Comment:   comment,
-			}
-			definitions = append(definitions, def)
+	case "arrow_function":
+		if def := extractArrowFunction(node, src, lines, filePath); def != nil {
+			*definitions = append(*definitions, *def)
+		}
+	case "class_declaration":
+		if def := extractClass(node, src, lines, filePath); def != nil {
+			*definitions = append(*definitions, *def)
+		}
+	case "method_definition":
+		if def := extractMethod(node, src, lines, filePath); def != nil {
+			*definitions = append(*definitions, *def)
 		}
 	}
 
-	return definitions, nil
+	for i := 0; i < int(node.ChildCount()); i++ {
+		child := node.Child(i)
+		walkTree(child, src, lines, filePath, definitions)
+	}
+}
+
+func extractFunction(node *sitter.Node, src []byte, lines []string, filePath string) *types.Definition {
+	nameNode := node.ChildByFieldName("name")
+	if nameNode == nil {
+		return nil
+	}
+	name := string(src[nameNode.StartByte():nameNode.EndByte()])
+	sig := string(src[node.StartByte():node.EndByte()])
+	line := int(node.StartPoint().Row) + 1
+	comment := extractJSComment(lines, line-1)
+	def := types.Definition{
+		Type:      "function",
+		Name:      name,
+		Line:       line,
+		LineEnd:    int(node.EndPoint().Row) + 1,
+		Id:        computeId(filePath, name, sig),
+		Signature: normalizeWhitespace(sig),
+		Comment:   comment,
+	}
+	return &def
+}
+
+func extractArrowFunction(node *sitter.Node, src []byte, lines []string, filePath string) *types.Definition {
+	parent := node.Parent()
+	if parent != nil && parent.Type() == "variable_declarator" {
+		nameNode := parent.ChildByFieldName("name")
+		if nameNode != nil {
+			name := string(src[nameNode.StartByte():nameNode.EndByte()])
+			declNode := parent.Parent()
+			if declNode != nil && declNode.Type() == "lexical_declaration" {
+				sig := string(src[declNode.StartByte():declNode.EndByte()])
+				line := int(declNode.StartPoint().Row) + 1
+				comment := extractJSComment(lines, line-1)
+				def := types.Definition{
+					Type:      "function",
+					Name:      name,
+					Line:       line,
+					LineEnd:    int(declNode.EndPoint().Row) + 1,
+					Id:        computeId(filePath, name, sig),
+					Signature: normalizeWhitespace(sig),
+					Comment:   comment,
+				}
+				return &def
+			}
+		}
+	}
+	return nil
+}
+
+func extractClass(node *sitter.Node, src []byte, lines []string, filePath string) *types.Definition {
+	nameNode := node.ChildByFieldName("name")
+	if nameNode == nil {
+		return nil
+	}
+	name := string(src[nameNode.StartByte():nameNode.EndByte()])
+	definition := string(src[node.StartByte():node.EndByte()])
+	line := int(node.StartPoint().Row) + 1
+	comment := extractJSComment(lines, line-1)
+	def := types.Definition{
+		Type:       "type",
+		Name:       name,
+		Line:        line,
+		LineEnd:     int(node.EndPoint().Row) + 1,
+		Id:         computeId(filePath, name, definition),
+		Definition: normalizeWhitespace(definition),
+		Comment:    comment,
+	}
+	return &def
+}
+
+func extractMethod(node *sitter.Node, src []byte, lines []string, filePath string) *types.Definition {
+	nameNode := node.ChildByFieldName("name")
+	if nameNode == nil {
+		return nil
+	}
+	name := string(src[nameNode.StartByte():nameNode.EndByte()])
+	sig := string(src[node.StartByte():node.EndByte()])
+	line := int(node.StartPoint().Row) + 1
+	comment := extractJSComment(lines, line-1)
+	def := types.Definition{
+		Type:      "function",
+		Name:      name,
+		Line:       line,
+		LineEnd:    int(node.EndPoint().Row) + 1,
+		Id:        computeId(filePath, name, sig),
+		Signature: normalizeWhitespace(sig),
+		Comment:   comment,
+	}
+	return &def
 }
 
 // extractJSComment extracts preceding comment lines
